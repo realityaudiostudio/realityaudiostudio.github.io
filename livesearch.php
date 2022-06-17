d09431cd0a60b9f2fbe12947941e31e37eab870f
Reality Audio Studio Dev <87328737+realityaudiostudio@users.noreply.github.com>
2022-06-17 10:38:10 +0530

    Update livesearch.php

--- livesearch.php
+++ livesearch.php
@@ -1,22 +1,45 @@
 <?php
-   $xml_doc = new DOMDocument();
-   $xml_doc->load('persons.xml');
+$xmlDoc=new DOMDocument();
+$xmlDoc->load("links.xml");
 
-   $x=$xml_doc->getElementsByTagName('name');
-   
-   $q = $_GET['q'];
-   $result = '';
-   foreach($x as $node) {
-       if (stripos("{$node->nodeValue}", $q) !== false) {
-           $result .= "{$node->nodeValue}";
-       }
-   }
+$x=$xmlDoc->getElementsByTagName('link');
 
-  // Set $response to "No records found." in case no hint was found
-  // or the values of the matching values
-   if ($result == '')
-       $result = 'No records found.';
+//get the q parameter from URL
+$q=$_GET["q"];
 
-  //show the results or "No records found."
-   echo $result;
+//lookup all links from the xml file if length of q>0
+if (strlen($q)>0) {
+  $hint="";
+  for($i=0; $i<($x->length); $i++) {
+    $y=$x->item($i)->getElementsByTagName('title');
+    $z=$x->item($i)->getElementsByTagName('url');
+    if ($y->item(0)->nodeType==1) {
+      //find a link matching the search text
+      if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
+        if ($hint=="") {
+          $hint="<a href='" .
+          $z->item(0)->childNodes->item(0)->nodeValue .
+          "' target='_blank'>" .
+          $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+        } else {
+          $hint=$hint . "<br /><a href='" .
+          $z->item(0)->childNodes->item(0)->nodeValue .
+          "' target='_blank'>" .
+          $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+        }
+      }
+    }
+  }
+}
+
+// Set output to "no suggestion" if no hint was found
+// or to the correct values
+if ($hint=="") {
+  $response="no suggestion";
+} else {
+  $response=$hint;
+}
+
+//output the response
+echo $response;
 ?>
